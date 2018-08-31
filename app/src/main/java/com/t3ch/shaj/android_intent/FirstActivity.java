7848107cd434ad4ea0d3bfb9d45c7a7f19362f83
SHAJ <shakilahmedshaj@gmail.com>
2018-08-31 19:50:05 +0600

    java code done

--- app/src/main/java/com/t3ch/shaj/android_intent/FirstActivity.java
+++ app/src/main/java/com/t3ch/shaj/android_intent/FirstActivity.java
@@ -1,13 +1,28 @@
 package com.t3ch.shaj.android_intent;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FirstActivity extends AppCompatActivity {
+
+    private Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        button1 = findViewById(R.id.BTN_1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/t3ch/shaj/android_intent/SecondActivity.java
+++ app/src/main/java/com/t3ch/shaj/android_intent/SecondActivity.java
@@ -1,13 +1,30 @@
 package com.t3ch.shaj.android_intent;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        button2 = findViewById(R.id.BTN_2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this, FirstActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
